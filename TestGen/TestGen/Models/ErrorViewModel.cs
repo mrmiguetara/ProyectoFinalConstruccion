27a7c4c73d33357139f6793ba471284096af9db0
MiguelAGonzalezJ <miguel.gonzalez@pitech.com.do>
2019-06-15 20:32:53 -0400

    Initial Commit

---
+++ TestGen/TestGen/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestGen.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
