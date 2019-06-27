d72468f71e69e0064bec011388046fed09fdc6d2
Juan DAniel Ozuna <juandanielozuna2@gmail.com>
2019-06-26 20:48:48 -0400

    Refactoring code

---
+++ TestGen/FrontEnd/src/views/Account/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <account-view>
+      <account-center-card>
+        <login-form></login-form>
+        <button class="btn btn-secondary btn-block">Register</button>
+      </account-center-card>
+    </account-view>
+  </div>
+</template>
+
+<script>
+import AccountView from '@/components/utilities/AccountView'
+import AccountCenterCard from "@/components/account/AccountCenterCard";
+import LoginForm from "@/components/account/LoginForm";
+
+export default {
+  name: "login",
+  components: {
+    AccountCenterCard,
+    AccountView,
+    LoginForm
+  }
+}
+</script>
+
+<style>
+
+</style>
