ce07754d17180880ab90a3413600b5fa6b91a449
Jose Arias <joseam2969@gmail.com>
2019-06-27 17:28:53 -0400

    Intento de Prueba de Integración

---
+++ TestGen/BackEnd/IntegrationTests/TodoRouteShould.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using TestGen.IntegrationTests;
+using Xunit;
+namespace AspNetCoreTodo.IntegrationTests
+{
+	public class TodoRouteShould
+	{
+		[Fact]
+		public void InitialPageLoadedCorrectly()
+		{
+			Thread.Sleep(800);
+			Assert.Equal(0, 0);
+		}
+	}
+}
